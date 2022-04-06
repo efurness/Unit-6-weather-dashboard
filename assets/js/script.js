3d495771f0d045444a855923c5744611fd00eca4
Ellen Furness <elasfern@gmail.com>
2022-04-06 00:13:02 -0600

    4-5-22jsimprovement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 var submitEl = document.getElementById("submitEl")
 var searchInput = document.getElementById("search-input");
+var userSearcher 
+var storage = JSON.parse(localStorage.getItem("search-input"))
+if (storage) userSearcher = storage 
+else userSearcher = []
 // add eventListerer to submit 
 submitEl.addEventListener("click", function (event) {
     event.preventDefault();
     var city = searchInput.value;
+// click new variable for city
+userSearcher.push(city)
+localStorage.setItem("search-input", JSON.stringify(userSearcher))
 
     var firstUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=c8f8824f82f862c7696a070f2a1a8586";
 
@@ -24,17 +31,19 @@ submitEl.addEventListener("click", function (event) {
                                 console.log(response);
                                 response.json().then(function (data) {
                                     console.log(data);
+                                    var uv = document.getElementById("uv1")
+                                    uv.textContent = data.uvi
                                     for (var i = 0; i < 6; i++) {
                                         var date = document.getElementById("date" + (i + 1))
                                         var temp = document.getElementById("temp" + (i + 1))
                                         var wind = document.getElementById("wind" + (i + 1))
-                                        var uv = document.getElementById("uv" + (i + 1))
+                                        
                                         var humid = document.getElementById("humid" + (i + 1))
-                                        date.innerText = "Date " + (i + 1)
-                                        temp.innerText = "Tempurature " + (i + 1)
-                                        wind.innerText = "Wind " + (i + 1)
-                                        uv.innerText = "UV index " + (i + 1)
-                                        humid.innerText = "Humidity " + (i + 1)
+                                        date.innerText = data.daily[i].dt
+                                        temp.innerText = data.daily[i].temp.day
+                                        wind.innerText = data.daily[i].wind_speed
+                                        // 
+                                        humid.innerText = data.daily[i].humidity
                                     }
                                     
                                 })
