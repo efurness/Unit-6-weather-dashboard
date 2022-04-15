293af94ceec7978054db1157791605ea74db2b10
Ellen Furness <elasfern@gmail.com>
2022-04-14 18:18:37 -0600

    4-14-temp

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ var submitEl = document.getElementById("submitEl")
 // declaring search input
 var searchInput = document.getElementById("search-input");
 // declare userSearcher and settin item to storage
-var userSearcher 
+var userSearcher
 var storage = JSON.parse(localStorage.getItem("search-input"))
-if (storage) userSearcher = storage 
+if (storage) userSearcher = storage
 else userSearcher = []
 
 // add eventListerer to submit 
@@ -13,13 +13,13 @@ submitEl.addEventListener("click", function (event) {
     event.preventDefault();
     var city = searchInput.value;
 
-// click new variable for city, pushing user search of city to storage using JSON stringify
-userSearcher.push(city)
-localStorage.setItem("search-input", JSON.stringify(userSearcher))
+    // click new variable for city, pushing user search of city to storage using JSON stringify
+    userSearcher.push(city)
+    localStorage.setItem("search-input", JSON.stringify(userSearcher))
 
-// establishing a url to get data from API open weather map for forecast and setting key to get data
+    // establishing a url to get data from API open weather map for forecast and setting key to get data
     var firstUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=c8f8824f82f862c7696a070f2a1a8586";
-// here using fetch response to get longitude and latitude for a particular city
+    // here using fetch response to get longitude and latitude for a particular city
     fetch(firstUrl)
         .then(function (response) {
             if (response.ok) {
@@ -28,7 +28,7 @@ localStorage.setItem("search-input", JSON.stringify(userSearcher))
                     console.log(data);
                     var long = data.city.coord.lon;
                     var lat = data.city.coord.lat;
-                    var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=hourly,minutely,alerts&appid=c8f8824f82f862c7696a070f2a1a8586";
+                    var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&exclude=hourly,minutely,alerts&appid=c8f8824f82f862c7696a070f2a1a8586";
 
                     // fetch API data for a second onecall with key
                     fetch(url)
@@ -40,12 +40,12 @@ localStorage.setItem("search-input", JSON.stringify(userSearcher))
                                     // getting UV data and adding uvi data to text content
                                     var uv = document.getElementById("uv1")
                                     uv.textContent = data.uvi
-                                    
-                // a for statement to get the 1 day forcast and next five days getting date, temp and wind
+
+                                    // a for statement to get the 1 day forcast and next five days getting date, temp and wind
                                     for (var i = 0; i < 6; i++) {
                                         // converting timestamp to new date
                                         var timestamp = data.daily[i].dt;
-                                        var newdate = new Date(timestamp*1000);
+                                        var newdate = new Date(timestamp * 1000);
                                         // getting element by id, and index plus 1
                                         var date = document.getElementById("date" + (i + 1))
                                         var temp = document.getElementById("temp" + (i + 1))
@@ -53,18 +53,18 @@ localStorage.setItem("search-input", JSON.stringify(userSearcher))
                                         var humid = document.getElementById("humid" + (i + 1))
                                         // putting data into innertext of cards that have 5 day forcast
                                         date.innerText = newdate.toLocaleDateString()
-                                        temp.innerText = "Temperature " + data.daily[i].temp.day + " Kelvin"
-                                        wind.innerText = "Wind speed " + data.daily[i].wind_speed  + " mph"
+                                        temp.innerText = "Temperature " + data.daily[i].temp.day + " Fahrenheit"
+                                        wind.innerText = "Wind speed " + data.daily[i].wind_speed + " mph"
                                         humid.innerText = data.daily[i].humidity + "%  Humidity"
                                     }
-                                    
+
                                 })
                             }
                         })
 
                 })
             }
-    })
+        })
 })
 
 // current
